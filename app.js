5b0265cba82afcec0aa05b2253de4d6cdfc2515b
kiradotomexubiraum-collab <kiradotomexubiraum@gmail.com>
2026-07-05 20:48:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3013,7 +3013,7 @@ async function init() {
   } else {
     clearAuth();
     screen = "login";
-    loginError = "Sua sessão expirou. Faça login novamente.";
+    loginError = "Sua sessão expirou. Faça login novamente!";
     render();
   }
 }
